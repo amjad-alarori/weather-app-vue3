27ff67390832805b0933086951dd591b37d74bcf
Amjad Alarori <amjad.alarori@itonomy.nl>
2023-02-26 14:12:27 +0100

    cleanning unneeded files

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'weather-icons/css/weather-icons.min.css';
 
 createApp(App).mount('#app')
